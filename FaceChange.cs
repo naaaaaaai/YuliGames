5969cd96464049d198d854b097f05bc5bf5eb2c1
naaaaaaai <61654946+naaaaaaai@users.noreply.github.com>
2021-03-22 15:48:46 +0900

    Vroid Expression

---
+++ FaceChange.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRM;
+
+public class FaceChange : MonoBehaviour
+{
+
+	private VRMBlendShapeProxy proxy;
+
+    
+    
+
+	// Use this for initialization
+	void Start()
+	{
+        proxy = this.GetComponent<VRMBlendShapeProxy>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+
+		if (proxy == null)
+		{
+            Debug.Log("null");
+			proxy = GetComponent<VRMBlendShapeProxy>();
+		}
+		else
+		{
+
+			//キーボード入力
+			if (Input.GetKey(KeyCode.D))
+			{
+                Debug.Log("yuri");
+				//表情呼び出し
+				proxy.SetValue(BlendShapePreset.Joy, 1.0f);
+				//proxy.SetValue(BlendShapePreset.Extra, 1.0f);
+
+			}
+			else
+			{
+
+				proxy.SetValue("Neutral", 0f);
+			}
+
+			if (Input.GetKey(KeyCode.DownArrow))
+			{
+				proxy.SetValue("Fun", 1.0f);
+
+			}
+			else
+			{
+				proxy.SetValue("Fun", 0f);
+			}
+
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				proxy.SetValue("Joy", 1.0f);
+			}
+			else
+			{
+				proxy.SetValue("Joy", 0f);
+			}
+
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				proxy.SetValue("Sorrow", 1.0f);
+			}
+			else
+			{
+				proxy.SetValue("Sorrow", 0f);
+			}
+
+		}
+
+
+
+	}
+}
---
+++ FaceKeep.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRM;
+
+public class FaceKeep : MonoBehaviour
+{
+	private VRMBlendShapeProxy proxy;
+
+	//3秒後に表情を戻す(別の表情にする)
+	float TimeCount = 4;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+
+		if (proxy == null)
+		{
+			proxy = GetComponent<VRMBlendShapeProxy>();
+		}
+		else
+		{
+
+			proxy.SetValue(BlendShapePreset.Joy, 1.0f);
+
+			//表情処理終章後にカウント開始
+			TimeCount -= Time.deltaTime;
+            if(TimeCount <=2){
+
+				proxy.SetValue(BlendShapePreset.Joy, 0f);
+				//表情変化
+				proxy.SetValue(BlendShapePreset.Fun, 1.0f);
+
+
+            }
+			//カウントが0になったら=この場合は3秒経ったら
+			if (TimeCount <= 0)
+			{
+
+				proxy.SetValue(BlendShapePreset.Fun, 0f);
+				//表情変化
+				proxy.SetValue(BlendShapePreset.Sorrow, 1.0f);
+
+			}
+
+
+
+		}
+
+
+
+
+	}
+}
